0741913ce646189448a2a5fde8de959cc2f22247
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-16 17:25:11 -0500

    Back button is working from detail page, but I don;t understand why my image url breaks the page

--- src/components/PokeDetailView.jsx
+++ src/components/PokeDetailView.jsx
@@ -1,47 +1,56 @@
 //Fetch details from selected Pokemon and display them here.
-//import useEffect
-//fetch pokemon api for specific pokemon
-//put that in state
-//put details in below
+  //import useEffect
+  //fetch pokemon api for specific pokemon
+  //put that in state
+  //put details in below
 //make back button
 //Extra Time? Style this to look like a real Pokemon card with eye-exploding colors.
 
 import { useEffect, useState } from "react";
 
-const PokeDetailView = ({ selectedPokemon}) => {
+const PokeDetailView = ({selectedPokemon, setSelectedPokemon}) =>{
   const [pokeDetails, setPokeDetails] = useState({});
 
-  console.log(selectedPokemon)
-  try {
-    useEffect(() => {
-      const fetchPokemonDetails = async () => {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
-        const data = await response.json();
-        setPokeDetails(data);
-      }
-      fetchPokemonDetails()
-    }, []);
-  }
-  catch (error) {
-    console.log("error", { error })
-  }
+//   console.log(pokeDetails, 'default state')
+// console.log('line 14', selectedPokemon)
 
-  return (
+ try{
+  
+  useEffect(() =>{
+    const fetchPokemonDetails = async () => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`);
+      const data = await response.json();
+      setPokeDetails(data);
+      console.log(pokeDetails)
+    }
+    fetchPokemonDetails()
+    
+  }, [pokeDetails]);
+}
+catch (error) {
+  console.log("error", error)
+} 
+
+console.log(pokeDetails)
+// const imageURL = pokeDetails.sprites.front_default
+
+  return(
     <>
       <h1>Name: {pokeDetails.name}</h1>
-
       <h3>Base Experience: {pokeDetails.base_experience}</h3>
       <h4>Weight: {pokeDetails.weight}</h4>
       <h5>Height: {pokeDetails.height} </h5>
-      <img src={pokeDetails.sprites.front_default} alt="pokemon" width="300px" />
+      {/* <img src={imageURL}/> */}
+      <button
+        onClick={() =>{
+          setSelectedPokemon(null);
+          setPokeDetails({});
+        }}
+      
+      >Back</button>
     </>
   )
 
 }
-  // {/* <button onClick={() => {
-  //       setSelectedPokemon(null);
-  //     }}>
-  //       Back to list
-  //     </button> */}
 
 export default PokeDetailView;
